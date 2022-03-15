6bf82789a175d9625a790b1886ef808737a68f94
franlm1 <franlm1@gmail.com>
2022-03-15 09:33:48 +0100

    listUser

---
+++ lib/list/list_user.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/cupertino.dart';
+
+import 'package:flutter/material.dart';
+
+class ListUser extends StatefulWidget {
+  @override
+  ListUserState createState() => ListUserState();
+}
+
+class ListUserState extends State<ListUser> {
+
+
+  
+  @override
+  void initState() {
+    super.initState();
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,44 @@
 import 'package:flutter/material.dart';
 
-import 'package:firebase_core/firebase_core.dart';
-
-
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  FirebaseApp firebaseApp = await Firebase.initializeApp();
-  runApp(MyApp());
+void main() {
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
- 
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        
-        home: Text("fdf"),
-        
-
+      debugShowCheckedModeBanner: false,
+      title: 'Users',
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+        brightness: Brightness.light,
+      ),
+      darkTheme: ThemeData(
+        primarySwatch: Colors.pink,
+        brightness: Brightness.light,
+      ),
+      home: Text("sdf"),
+      routes: <String, WidgetBuilder>{},
     );
   }
 }
 
+class HomePageMain extends StatefulWidget {
+  @override
+  _SearchListState createState() => new _SearchListState();
+}
+
+class _SearchListState extends State<HomePageMain> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+
+
+    );
+  }
+}
